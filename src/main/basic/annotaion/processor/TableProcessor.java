521decd434496e0362b2320f2c0af04c0e314fc0
androidjp <1049578026@qq.com>
2016-11-15 15:13:06 +0800

    注解demo修改

--- src/main/basic/annotaion/processor/TableProcessor.java
+++ src/main/basic/annotaion/processor/TableProcessor.java
@@ -21,11 +21,11 @@ public class TableProcessor implements IProcessor {
     public String process(String url) throws Exception {
         ///① 利用工具类，深度优先遍历的形式获取所有的文件
         List<File> classFiles = ClassFileUtils.getClassFiles(url);
-        ///② 新建缓存类
         StringBuilder sql = new StringBuilder();
-        ///③ 对所有的文件都进行类的获取，转成类列表
+        ///② 对每一个文件都进行类的获取，转成类列表
         for (File file : classFiles){
             Class<?> clazz = ClassFileUtils.loadClass(file);
+            ///③ 分析Class类中的注解，并生成SQL语句，存储到StringBuilder中
             TableInfo tableInfo = new TableInfo();
             tableInfo = tableInfo.parse(clazz);
             if (tableInfo!=null){
@@ -34,6 +34,4 @@ public class TableProcessor implements IProcessor {
         }
         return sql.toString();
     }
-
-
 }
